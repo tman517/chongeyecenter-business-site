a758b6b71ba7ac9542aaf5c74e46c9cf63b49835
Tony Fang <tonyfang@tonys-mbp.attlocal.net>
2017-03-20 22:04:00 -0700

    responsive banner

--- js/main.js
+++ js/main.js
@@ -8,15 +8,34 @@ $(document).ready(function(){
 
 function initHeader(){
 	$(window).bind("scroll", function(){
-		var windowScrollTop = $(window).scrollTop();
+		resizeHeader();
+	});
 
+	$(window).bind("resize", function(){
+		resizeHeader();
+	});
+
+	resizeHeader();
+}
+
+function resizeHeader(){
+	var windowScrollTop = $(window).scrollTop();
+	var availWidth = $(window).width();
+
+	if(availWidth < 900){
+		$("header").addClass("sticky");
+		$(".banner").addClass("sticky");
+	}
+	else{
 		if(windowScrollTop > 50){
 			$("header").addClass("sticky");
 		}
 		else {
 			$("header").removeClass("sticky");
 		}
-	});
+
+		$(".banner").removeClass("sticky");
+	}
 }
 
 function initNav(){
